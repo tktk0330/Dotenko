321597347b2ee0bb92c1652c4488678b03bd2071
tktk0330 <tkmsnd0330@gmail.com>
2024-12-21 00:31:38 +0900

    Admob Pods追加

---
+++ Pods/Target Support Files/Pods-Dotenko-DotenkoUITests/Pods-Dotenko-DotenkoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Dotenko_DotenkoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Dotenko_DotenkoUITests
+@end
---
+++ Pods/Target Support Files/Pods-DotenkoTests/Pods-DotenkoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DotenkoTests : NSObject
+@end
+@implementation PodsDummy_Pods_DotenkoTests
+@end
